8ed13ee1e9414762cf8e3fe582f3aa89d8ab5fc2
zeyucui1 <zeyu.cui@student.uts.edu.au>
2024-04-14 17:57:30 +1000

    feat: Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
-dotenv.config()
+import connectDB from './config/db.js'
 
-const port = process.env.PORT || 5000 
+dotenv.config()
+connectDB()
+const port = process.env.PORT || 5000
 
 const app = express()
 

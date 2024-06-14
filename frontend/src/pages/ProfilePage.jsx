7f941fc2d31cec09300dc0c43de9ee892d99e562
zeyucui1 <zeyu.cui@student.uts.edu.au>
2024-06-14 14:19:16 +1000

    feat: complete userProfile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import { useProfileMutation } from '../slices/usersApiSlice'
-import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table, Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
+import { FaTimes } from 'react-icons/fa'
+
+import { toast } from 'react-toastify'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { toast } from 'react-toastify'
+import { useProfileMutation } from '../slices/usersApiSlice'
+import { useGetMyOrdersQuery } from '../slices/orderApiSlice'
 import { setCredentials } from '../slices/authslice'
+import { Link } from 'react-router-dom'
 
-const ProfilePage = () => {
+const ProfileScreen = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const dispatch = useDispatch()
+
   const { userInfo } = useSelector((state) => state.auth)
-  const [updateProfile, { isLoading: loadingUpdateProfile, isError, error }] =
+
+  const { data: orders, isLoading, error } = useGetMyOrdersQuery()
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
     useProfileMutation()
+
   useEffect(() => {
-    if (userInfo) {
-      setName(userInfo.name)
-      setEmail(userInfo.email)
-    }
-  }, [userInfo])
+    setName(userInfo.name)
+    setEmail(userInfo.email)
+  }, [userInfo.email, userInfo.name])
+
+  const dispatch = useDispatch()
   const submitHandler = async (e) => {
     e.preventDefault()
     if (password !== confirmPassword) {
@@ -41,56 +48,117 @@ const ProfilePage = () => {
       }
     }
   }
+
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
+
         <Form onSubmit={submitHandler}>
-          <Form.Group controlId="name" className="my-2">
+          <Form.Group className="my-2" controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control
-              type="name"
-              placeholder="Enter user name"
+              type="text"
+              placeholder="Enter name"
               value={name}
               onChange={(e) => setName(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          <Form.Group controlId="email" className="my-2">
+
+          <Form.Group className="my-2" controlId="email">
             <Form.Label>Email Address</Form.Label>
             <Form.Control
               type="email"
-              placeholder="Enter user email"
+              placeholder="Enter email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          <Form.Group controlId="password" className="my-2">
+
+          <Form.Group className="my-2" controlId="password">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
-              placeholder="Enter user password"
+              placeholder="Enter password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          <Form.Group controlId="confirmPassword" className="my-2">
+
+          <Form.Group className="my-2" controlId="confirmPassword">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
-              placeholder="Confirm user password"
+              placeholder="Confirm password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          <Button type="submit" variant="primary" className="my-2">
-            Update Profile
+
+          <Button type="submit" variant="primary">
+            Update
           </Button>
           {loadingUpdateProfile && <Loader />}
         </Form>
       </Col>
-      <Col md={9}></Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    <Button
+                      as={Link}
+                      to={`/order/${order._id}`}
+                      className="btn-sm"
+                      variant="light"
+                    >
+                      Details
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Col>
     </Row>
   )
 }
 
-export default ProfilePage
+export default ProfileScreen

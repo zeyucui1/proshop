949d9a4db9cfd0b52fc6cec2f3a9e3a84ae6202c
zeyucui1 <zeyu.cui@student.uts.edu.au>
2024-06-20 18:24:48 +1000

    feat: Refactor OrderPage and OrderListPage components

--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
-import { FaTrash, FaEdit } from 'react-icons/fa'
+import { FaTrash, FaEdit, FaPlus } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
 import ConfirmModal from '../../components/ConfirmModal'
+import Paginate from '../../components/Paginate'
+import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import {
   useGetProductsQuery,
@@ -13,121 +14,134 @@ import {
 } from '../../slices/productsApiSlice'
 
 const ProductListPage = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery()
-  const [createProduct, { isLoading: loadingCreate }] =
-    useCreateProductMutation()
+  const { pageNumber } = useParams()
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({
+    pageNumber,
+  })
+
   const [deleteProduct, { isLoading: loadingDelete }] =
     useDeleteProductMutation()
 
-  const [showModal, setShowModal] = useState(false)
-  const [modalAction, setModalAction] = useState(null)
-  const [selectedProductId, setSelectedProductId] = useState(null)
+  const [createProduct, { isLoading: loadingCreate }] =
+    useCreateProductMutation()
 
-  const handleDelete = (id) => {
-    setSelectedProductId(id)
-    setModalAction('delete')
-    setShowModal(true)
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
+  const [confirmModalInfo, setConfirmModalInfo] = useState({
+    id: null,
+    action: null,
+    title: '',
+    body: '',
+  })
+
+  const handleCloseConfirmModal = () => setShowConfirmModal(false)
+
+  const handleShowConfirmModal = (id, action, title, body) => {
+    setConfirmModalInfo({ id, action, title, body })
+    setShowConfirmModal(true)
   }
 
-  const handleCreate = () => {
-    setModalAction('create')
-    setShowModal(true)
-  }
-
-  const handleClose = () => {
-    setShowModal(false)
-  }
-
-  const handleConfirm = async () => {
-    if (modalAction === 'delete') {
-      try {
-        await deleteProduct(selectedProductId).unwrap()
-        refetch()
-        toast.success('Product deleted successfully')
-      } catch (error) {
-        toast.error(error?.data?.message || error.message)
-      }
-    } else if (modalAction === 'create') {
-      try {
-        await createProduct().unwrap()
-        refetch()
-        toast.success('Product created successfully')
-      } catch (error) {
-        toast.error(error?.data?.message || error.message)
-      }
+  const handleConfirmAction = async () => {
+    const { id, action } = confirmModalInfo
+    try {
+      await action(id)
+      refetch()
+      toast.success('A new Product create successfully!')
+    } catch (err) {
+      toast.error(err?.data?.message || err.error)
+    } finally {
+      handleCloseConfirmModal()
     }
-    setShowModal(false)
+  }
+
+  const deleteHandler = (id) => {
+    handleShowConfirmModal(
+      id,
+      deleteProduct,
+      'Confirm Deletion',
+      'Are you sure you want to delete this product?'
+    )
+  }
+
+  const createProductHandler = () => {
+    handleShowConfirmModal(
+      null,
+      createProduct,
+      'Confirm Creation',
+      'Are you sure you want to create a new product?'
+    )
   }
 
   return (
     <>
-      <Row>
+      <Row className="align-items-center">
         <Col>
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-          <Button className="btn-sm m-3" onClick={handleCreate}>
-            <FaEdit /> Create Product
+          <Button className="my-3" onClick={createProductHandler}>
+            <FaPlus /> Create Product
           </Button>
         </Col>
       </Row>
+
       {loadingCreate && <Loader />}
       {loadingDelete && <Loader />}
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message variant="danger">{error.data.message}</Message>
       ) : (
-        <Table striped hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>PRICE</th>
-              <th>CATEGORY</th>
-              <th>BRAND</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product._id}>
-                <td>{product._id}</td>
-                <td>{product.name}</td>
-                <td>${product.price}</td>
-                <td>{product.category}</td>
-                <td>{product.brand}</td>
-                <td>
-                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                    <Button variant="light" className="btn-sm">
+        <>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <Button
+                      as={Link}
+                      to={`/admin/product/${product._id}/edit`}
+                      variant="light"
+                      className="btn-sm mx-2"
+                    >
                       <FaEdit />
                     </Button>
-                  </LinkContainer>
-                  <Button
-                    variant="danger"
-                    className="btn-sm"
-                    onClick={() => handleDelete(product._id)}
-                  >
-                    <FaTrash style={{ color: 'white' }} />
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash style={{ color: 'white' }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
+        </>
       )}
       <ConfirmModal
-        show={showModal}
-        handleClose={handleClose}
-        handleConfirm={handleConfirm}
-        title={
-          modalAction === 'delete' ? 'Confirm Deletion' : 'Confirm Creation'
-        }
-        body={
-          modalAction === 'delete'
-            ? 'Are you sure you want to delete this product?'
-            : 'Are you sure you want to create a new product?'
-        }
+        show={showConfirmModal}
+        handleClose={handleCloseConfirmModal}
+        handleConfirm={handleConfirmAction}
+        title={confirmModalInfo.title}
+        body={confirmModalInfo.body}
       />
     </>
   )
